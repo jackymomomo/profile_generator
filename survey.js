1c6092f0d0065f07c699e283938ef9245bc82f51
jackymomomo <jacklloydrobinson@gmail.com>
2022-11-24 03:16:44 -0800

    a basic survey for anyone who wants to see if there as cool as me XD dont worry you dont have to take it i know you not

---
+++ survey.js
@@ -0,0 +1,33 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Whats your name? Nicknames are also cool ', (answer) => {
+  console.log(`nice to meet you ${answer}`);
+
+  rl.question("What's an activity you like doing? ", (answer) => {
+    console.log(`I like ${answer} to man thats sick`)
+
+    rl.question("Man we are so alike Whats your favourite music album 🎸 ", (answer) => {
+      console.log('gotta say I love cage the Elepahant')
+
+      rl.question("If you could only have one food the rest of your life what would it be? ", (answer) => {
+        console.log(`XD ${answer} is a good answer`)
+
+        rl.question("Whats your favourtie sport? ", (answer) => {
+          console.log(`I like basketball gets a good workout but ${answer} is cool too`)
+
+          rl.question("What superpower would you choose ", (answer) => {
+            console.log(`WHOA! ${answer}! damn I would prolly choose teleportation`)
+          
+          rl.close()
+          })
+        })
+      })
+    })
+  })
+});
